73f27d4b74a57611f2aaed0d25ef2ae086b33b46
Tom Augspurger <tom.w.augspurger@gmail.com>
2017-12-27 11:08:14 -0600

    more travis

--- ci/install-travis.sh
+++ ci/install-travis.sh
@@ -4,7 +4,7 @@ echo "[install-travis]"
 
 # install iniconda
 MINICONDA_DIR="$HOME/miniconda3"
-time wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh || exit 1
+time wget -q http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh || exit 1
 time bash miniconda.sh -b -p "$MINICONDA_DIR" || exit 1
 
 echo
