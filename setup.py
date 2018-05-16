82279f7002d4e68d84e686f04376ffbe9a56dd6b
Tom Augspurger <TomAugspurger@users.noreply.github.com>
2018-05-16 13:32:27 -0500

    PKG: Require six and ipaddress (#26)

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup, find_packages
 from os import path
 from codecs import open
+import sys
 
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+install_requires = ['pandas>=0.23.0', 'six']
+
+if sys.version_info.major == 2:
+    install_requires.append('ipaddress')
 
 setup(
     name='cyberpandas',
@@ -28,7 +33,5 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
     packages=find_packages(),
-    install_requires=[
-        'pandas>=0.23.0',
-    ]
+    install_requires=install_requires,
 )
